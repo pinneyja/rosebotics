b4705dd78bf66b0a0b4dbc562159f1282729bb44
pinneyja <pinneyja@rose-hulman.edu>
2018-10-01 11:01:47 -0400

    PEP8 formatting

--- src/rosebotics.py
+++ src/rosebotics.py
@@ -1,6 +1,6 @@
 """
   Capstone Project.  Code shared by the team.
-  Team members:  PUT_YOUR_NAMES_HERE.
+  Team members:  Jacob Pinney, Hunter Hicks, and Myon McGee.
   Fall term, 2018-2019.
 """
 
