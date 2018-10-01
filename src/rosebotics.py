c79c4db3cfde22805b13d74a7cad47f4e5f7456c
pinneyja <pinneyja@rose-hulman.edu>
2018-10-01 11:36:11 -0400

    fixed rosebotics stop method

--- src/rosebotics.py
+++ src/rosebotics.py
@@ -18,16 +18,16 @@ class Snatch3rRobot(object):
     def __init__(self,
                  left_wheel_port = ev3.OUTPUT_B,
                  right_wheel_port = ev3.OUTPUT_C):
-        self.left_motor = Wheel(left_wheel_port)
-        self.right_motor = Wheel(right_wheel_port)
+        self.left_wheel = Wheel(left_wheel_port)
+        self.right_wheel = Wheel(right_wheel_port)
 
     def go(self, left_duty_cycle_percent, right_duty_cycle_percent):
-        self.left_motor.start_spinning(left_duty_cycle_percent)
-        self.right_motor.start_spinning(right_duty_cycle_percent)
+        self.left_wheel.start_spinning(left_duty_cycle_percent)
+        self.right_wheel.start_spinning(right_duty_cycle_percent)
 
     def stop(self, stop_action=StopAction.BRAKE.value):
-        self.left_motor.start_spinning(stop_action)
-        self.right_motor.start_spinning(stop_action)
+        self.left_wheel.stop_spinning(stop_action)
+        self.right_wheel.stop_spinning(stop_action)
 
 
 
